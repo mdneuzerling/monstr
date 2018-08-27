2fc8c5897bbcb265425c6ec69f749cfe0ee9d113
Murray David Neuzerling <murray@neuzerling.com>
2018-08-27 21:07:31 +1000

    Added monsters data set

---
+++ R/monsters.R
@@ -0,0 +1,20 @@
+#' @importFrom tibble tibble
+NULL
+
+#' Monsters
+#'
+#' Monsters in the 5th edition SRD. Data accessible via the   
+#' (\href{http://dnd.wizards.com/articles/features/systems-reference-document-srd}{Open Gaming Licence Version 1.0a}
+#'
+#' @docType data
+#'
+#' @format A tibble
+#' 
+#' @usage data(monstr)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(monsters)
+#' head(monsters)
+"monsters"
